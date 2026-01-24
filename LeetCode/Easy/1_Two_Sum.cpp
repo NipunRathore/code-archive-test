f5614629deb0b1059632c68389e50f2d20629369
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-01-24 14:40:03 +0530

    Add solution: Two Sum (LeetCode)

--- LeetCode/Easy/1_Two_Sum.cpp
+++ LeetCode/Easy/1_Two_Sum.cpp
@@ -1,10 +1,27 @@
 /**
  * Problem: Two Sum
  * Platform: LeetCode
- * Problem URL: https://leetcode.com/problems/two-sum/submissions/1895131449/
+ * Problem URL: https://leetcode.com/problems/two-sum/
  * Difficulty: Easy
  * Language: cpp
- * Date: 2026-01-24T08:50:11.981Z
+ * Date: 2026-01-24T09:10:01.825Z
  */
 
-cc
+class Solution {class Solution {
+public:public:
+    vector<int> twoSum(vector<int>& nums, int target) {    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> elementIndex ;        vector<int> elementIndex ;
+        for (int i =0 ; i<nums.size();i++)        for (int i =0 ; i<nums.size();i++)
+        {        {
+            for (int j=i+1;j<nums.size();j++)            for (int j=i+1;j<nums.size();j++)
+            {            {
+                if (nums[i]+nums[j]==target)                if (nums[i]+nums[j]==target)
+                {                {
+                    elementIndex.push_back(i);                    elementIndex.push_back(i);
+                    elementIndex.push_back(j);                    elementIndex.push_back(j);
+                    // elementIndex[0]=i;                    // elementIndex[0]=i;
+                    // elementIndex[1]=j;                    // elementIndex[1]=j;
+                    // break;                    // break;
+                                        
+                }                }
+            }            }
