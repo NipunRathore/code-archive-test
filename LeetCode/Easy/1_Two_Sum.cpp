d5f80b87c987a76d9c0f163598928ae2f9ceb2af
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-01-24 14:14:28 +0530

    Add solution: Two Sum (LeetCode)

---
+++ LeetCode/Easy/1_Two_Sum.cpp
@@ -0,0 +1,10 @@
+/**
+ * Problem: Two Sum
+ * Platform: LeetCode
+ * Problem URL: https://leetcode.com/problems/two-sum/submissions/777891683/
+ * Difficulty: Easy
+ * Language: cpp
+ * Date: 2026-01-24T08:44:05.842Z
+ */
+
+cc
