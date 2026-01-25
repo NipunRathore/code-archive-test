64f74571d370afc8cc0c6adebb3796dbf15a0193
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-01-25 16:11:25 +0530

    Add solution: Two Sum (LeetCode)

--- LeetCode/Easy/1_Two_Sum.cpp
+++ LeetCode/Easy/1_Two_Sum.cpp
@@ -4,17 +4,10 @@
  * Problem URL: https://leetcode.com/problems/two-sum/
  * Difficulty: Easy
  * Language: cpp
- * Date: 2026-01-24T09:12:10.389Z
+ * Date: 2026-01-25T10:41:23.541Z
  */
 
-class Solution {class Solution {
-public:public:
-    vector<int> twoSum(vector<int>& nums, int target) {    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> elementIndex ;        vector<int> elementIndex ;
-        for (int i =0 ; i<nums.size();i++)        for (int i =0 ; i<nums.size();i++)
-        {        {
-            for (int j=i+1;j<nums.size();j++)            for (int j=i+1;j<nums.size();j++)
-            {            {
+{            {
                 if (nums[i]+nums[j]==target)                if (nums[i]+nums[j]==target)
                 {                {
                     elementIndex.push_back(i);                    elementIndex.push_back(i);
@@ -22,6 +15,13 @@ public:public:
                     // elementIndex[0]=i;                    // elementIndex[0]=i;
                     // elementIndex[1]=j;                    // elementIndex[1]=j;
                     // break;                    // break;
-                                        
                 }                }
             }            }
+        {        {
+            for (int j=i+1;j<nums.size();j++)            for (int j=i+1;j<nums.size();j++)
+        vector<int> elementIndex ;        vector<int> elementIndex ;
+        for (int i =0 ; i<nums.size();i++)        for (int i =0 ; i<nums.size();i++)
+    vector<int> twoSum(vector<int>& nums, int     vector<int> twoSum(vector<int>& nums, int 
+target) {target) {
+class Solution {class Solution {
+public:public:
