7721617b3bc0e5f55940715608617af1f9f4e66b
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-01-26 19:19:57 +0530

    Add: Two Sum (C++) [Easy]

--- LeetCode/Easy/1_Two_Sum/1_Two_Sum.cpp
+++ LeetCode/Easy/1_Two_Sum/1_Two_Sum.cpp
@@ -2,10 +2,36 @@
  * Problem: Two Sum
  * Platform: LeetCode
  * Problem URL: https://leetcode.com/problems/two-sum/
+ * Difficulty: Easy
  * Language: C++
- * Date: 2026-01-26T13:47:26.179Z
+ * Date: 2026-01-26T13:49:55.534Z
  * Performance:
- * Runtime: 144ms (Beats 5.00%)
- * Memory: 14.17MB (Beats 66.62%)
+ * Runtime: 138ms (Beats 5.00%)
+ * Memory: 14.15MB (Beats 66.62%)
  */
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> elementIndex ;
+        for (int i =0 ; i<nums.size();i++)
+        {
+            for (int j=i+1;j<nums.size();j++)
+            {
+                if (nums[i]+nums[j]==target)
+                {
+                    elementIndex.push_back(i);
+                    elementIndex.push_back(j);
+                    // elementIndex[0]=i;
+                    // elementIndex[1]=j;
+                    // break;
+                }
+            }
+        }
+        return elementIndex;
+    }
+};
+// T.C. = O(N^2)
+// Testing Code Archive extension by @NipunRathore
+// solution + readme + images + stats
+// testing ui now 
