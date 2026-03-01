3252e3c782981b10ba4982bb908659ac8fcb3a2a
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-03-01 10:43:33 +0530

    solve(codeforces): 50A - Domino piling in cpp

---
+++ CodeForces/50A_50A_-_Domino_piling/50A_50A_-_Domino_piling.cpp
@@ -0,0 +1,10 @@
+/**
+ * Problem: 50A - Domino piling
+ * Platform: CodeForces
+ * Problem URL: https://codeforces.com/problemset/problem/50/A
+ * Language: cpp
+ * Date: 2026-03-01T05:13:30.992Z
+ */
+
+// Code for submission 364909172
+// Visit: https://codeforces.com/problemset/submission/364909172 to view
