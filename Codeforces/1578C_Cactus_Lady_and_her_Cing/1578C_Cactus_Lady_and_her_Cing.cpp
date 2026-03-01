2da94b3b9509d4b9d9aaca411824db2b842edc60
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-03-01 12:38:20 +0530

    solve(codeforces): Cactus Lady and her Cing in cpp - 358ms, 58900KB

---
+++ Codeforces/1578C_Cactus_Lady_and_her_Cing/1578C_Cactus_Lady_and_her_Cing.cpp
@@ -0,0 +1,13 @@
+/**
+ * Problem: Cactus Lady and her Cing
+ * Platform: Codeforces
+ * Problem URL: https://codeforces.com/problemset/problem/1578/C
+ * Tags: *3500
+ * Language: cpp
+ * Date: 2026-03-01T07:08:17.994Z
+ * Performance:
+ * Runtime: 358ms
+ * Memory: 58900KB
+ */
+
+// Code could not be extracted from submission page
