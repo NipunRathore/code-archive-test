f0f12162b1d3eb3993d2a8a45113af44ae4d3844
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-03-01 20:00:39 +0530

    solve(codeforces): Domino piling in cpp [800-1199] - 92ms, 0KB

---
+++ Codeforces/800-1199/50A_Domino_piling/50A_Domino_piling.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem: Domino piling
+ * Platform: Codeforces
+ * Problem URL: https://codeforces.com/problemset/problem/50/A
+ * Difficulty: 800-1199
+ * Tags: greedy, math, *800
+ * Language: cpp
+ * Date: 2026-03-01T14:30:37.066Z
+ * Performance:
+ * Runtime: 92ms
+ * Memory: 0KB
+ */
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    cout << M * N / 2 << endl;
+    return 0;
+}
+// code archive test - 13
