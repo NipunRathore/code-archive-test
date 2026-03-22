9e0d10b17678c08c16334f6d67b2f55399b2eb24
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-03-22 16:38:47 +0530

    solve(codeforces): Watermelon in cpp [800-1199]

---
+++ Codeforces/800-1199/4A_Watermelon/4A_Watermelon.cpp
@@ -0,0 +1,14 @@
+/**
+ * Problem: Watermelon
+ * Platform: Codeforces
+ * Problem URL: https://codeforces.com/problemset/problem/4/A
+ * Difficulty: 800-1199
+ * Tags: brute force, math, *800
+ * Language: cpp
+ * Date: 2026-03-22T11:08:45.439Z
+ * Performance:
+ * Runtime: 92ms
+ * Memory: 0KB
+ */
+
+// Code could not be extracted from submission page
