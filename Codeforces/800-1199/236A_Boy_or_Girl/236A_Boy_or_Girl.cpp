91af09f21bcb113876ad581eb7cb77093846543f
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-03-22 21:38:08 +0530

    Add: Boy or Girl (cpp) [800-1199] - 184ms, 100KB

---
+++ Codeforces/800-1199/236A_Boy_or_Girl/236A_Boy_or_Girl.cpp
@@ -0,0 +1,14 @@
+/**
+ * Problem: Boy or Girl
+ * Platform: Codeforces
+ * Problem URL: https://codeforces.com/problemset/problem/236/A
+ * Difficulty: 800-1199
+ * Tags: brute force, implementation, strings, *800
+ * Language: cpp
+ * Date: 2026-03-22T16:08:06.636Z
+ * Performance:
+ * Runtime: 184ms
+ * Memory: 100KB
+ */
+
+// Code could not be extracted from submission page
