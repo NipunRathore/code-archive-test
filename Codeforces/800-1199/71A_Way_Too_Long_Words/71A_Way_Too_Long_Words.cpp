eceb6f60c4945d3bcb584c82cae86e470476e2b4
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2026-03-22 21:38:09 +0530

    Add: Way Too Long Words (cpp) [800-1199] - 46ms, 0KB

---
+++ Codeforces/800-1199/71A_Way_Too_Long_Words/71A_Way_Too_Long_Words.cpp
@@ -0,0 +1,14 @@
+/**
+ * Problem: Way Too Long Words
+ * Platform: Codeforces
+ * Problem URL: https://codeforces.com/problemset/problem/71/A
+ * Difficulty: 800-1199
+ * Tags: strings, *800
+ * Language: cpp
+ * Date: 2026-03-22T16:08:07.649Z
+ * Performance:
+ * Runtime: 46ms
+ * Memory: 0KB
+ */
+
+// Code could not be extracted from submission page
